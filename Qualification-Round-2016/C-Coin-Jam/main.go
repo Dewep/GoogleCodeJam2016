f27b7d02ad1367991e163496cab42f0ddf4108fb
Maigret Aurélien <dewep.net@gmail.com>
2016-04-09 15:01:30 +0200

    Fix Problem C

--- Qualification-Round-2016/C-Coin-Jam/main.go
+++ Qualification-Round-2016/C-Coin-Jam/main.go
@@ -2,79 +2,66 @@ package main
 
 import "fmt"
 import "strconv"
-import "math"
+import "math/big"
 
-func is_prime(n int) bool {
-    if n < 2 {
-        return false
-    }
-    if n == 2 {
-        return true
-    }
-    if n & 1 == 0 {
-        return false
-    }
-    s := int(math.Sqrt(float64(n)))
-    for i := 3; i <= s; i += 2 {
-        if n % i == 0 {
-            return false
-        }
-    }
-    return true
-}
+func parse_number_base(jamcoin string, base int) int64 {
+    bi := big.NewInt(0)
+    _, is_success := bi.SetString(jamcoin, base)
 
-func parse_number_base(jamcoin string, base int) int {
-    value, err := strconv.ParseInt(jamcoin, base, 64)
-
-    if err != nil {
+    if !is_success {
+        fmt.Println("ERROR Parse number")
         return -1
     }
 
-    return int(value)
+    return bi.Int64()
 }
 
-func format_number_base(number int, base int) string {
-    return strconv.FormatInt(int64(number), base)
+func format_number_base(number int64, base int) string {
+    return strconv.FormatInt(number, base)
 }
 
-func is_jamcoin(jamcoin string, N int) bool {
+func check_jamcoin(jamcoin string, N int) (bool, string) {
     if len(jamcoin) != N || jamcoin[0] != '1' || jamcoin[len(jamcoin) - 1] != '1' {
-        return false
+        return false, ""
     }
 
-    for i := 2; i <= 10; i++ {
-        number := parse_number_base(jamcoin, i)
+    result := ""
 
-        if number == -1 || is_prime(number) {
-            return false
+    for base := 2; base <= 10; base++ {
+        number := parse_number_base(jamcoin, base)
+
+        div := int64(2)
+        for number % div != 0 {
+            div += 1
+            if div >= 100 {
+                return false, ""
+            }
         }
+        result += " " + strconv.Itoa(int(div))
     }
 
-    return true
+    return true, result
 }
 
-func get_min_number_jamcoin(N int, J int) int {
-    var jamcoin string
+func pow_int(number int, pow int) int64 {
+    result := int64(number)
 
-    for n := 0; n < N; n++ {
-        if n == 0 || n == N - 1 {
-            jamcoin += "1"
-        } else {
-            jamcoin += "0"
-        }
+    for i := 0; i < pow; i++ {
+        result *= int64(number)
     }
 
-    return parse_number_base(jamcoin, 2)
+    return result
 }
 
 func generate_jamcoin(N int, J int) {
-    number := get_min_number_jamcoin(N, J)
+    number := pow_int(2, N - 2) + 1
 
     for j := 0; j < J; {
         jamcoin := format_number_base(number, 2)
 
-        if is_jamcoin(jamcoin, N) {
-            fmt.Println(jamcoin)
+        is_jamcoin, result := check_jamcoin(jamcoin, N)
+        if is_jamcoin {
+            fmt.Println(jamcoin + result)
             j += 1
         }
 
