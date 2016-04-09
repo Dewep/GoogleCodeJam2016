63b5d3a763b4bf1a73be151120d5cefed5d713f8
Maigret Aurélien <dewep.net@gmail.com>
2016-04-09 10:36:47 +0200

    Problem A

---
+++ Qualification-Round-2016/A-Counting-Sheep/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func sheep(N int) int {
+    if N <= 0 {
+        return -1
+    }
+
+    set := make(map[int]bool)
+    for i := 0; i <= 9; i++ {
+        set[i] = true
+    }
+
+    count := 0
+    for len(set) > 0 {
+        count += 1
+        number := count * N
+
+        for number != 0 {
+            digit := number % 10
+            delete(set, digit)
+            number /= 10
+        }
+    }
+
+    return count * N
+}
+
+func main() {
+    var T int
+    fmt.Scan(&T)
+
+    for i := 1; i <= T; i++ {
+        var N int
+        fmt.Scan(&N)
+        count := sheep(N)
+
+        if count <= -1 {
+            fmt.Printf("Case #%d: INSOMNIA\n", i)
+        } else {
+            fmt.Printf("Case #%d: %d\n", i, count)
+        }
+    }
+}
